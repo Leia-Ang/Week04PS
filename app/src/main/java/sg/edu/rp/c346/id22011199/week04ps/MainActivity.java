e5a7e5a2bd489342f978f6c15359c0bc0b7bf1b4
Leia Ang <22011199@myrp.edu.sg>
2023-05-11 23:23:02 +0800

    Reservation

--- app/src/main/java/sg/edu/rp/c346/id22011199/week04ps/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22011199/week04ps/MainActivity.java
@@ -3,12 +3,15 @@ package sg.edu.rp.c346.id22011199.week04ps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,57 @@ public class MainActivity extends AppCompatActivity {
         tvSize = findViewById(R.id.textViewSize);
         etSize = findViewById(R.id.editTextSize);
         cbSmoke = findViewById(R.id.checkBoxSmoke);
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        buttonReset = findViewById(R.id.buttonReset);
+        buttonReserve = findViewById(R.id.buttonReserve);
+
+
+        buttonReserve.setOnClickListener(new View.OnClickListener() {
+
+            String Name = String.valueOf(etName.getText());
+            String number = String.valueOf(etTele.getText());
+            String size = String.valueOf(etSize.getText());
+            String time = String.valueOf(tp.getCurrentHour() + tp.getCurrentMinute());
+            String date = String.valueOf(dp.getMonth() + dp.getDayOfMonth() + dp.getYear());
+
+            if( cbSmoke.isChecked()){
+                String smokeArea = "Smoking Area";
+            }
+            else{
+                String smokeArea= "Non-smoking area";
+            }
+
+
+            String output = "Name : " + Name + ", Telephone: " + number + ", Size: " + size + smokeArea + ", Date: "+ date + ",Time: " + time;
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,output, Toast.LENGTH_SHORT).show();
+
+
+
+            }
+        });
+
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvName.setText("");
+                tvTele.setText(0);
+                tvSize.setText(0);
+                cbSmoke.setChecked(false);
+                dp.updateDate(2023, 6,1);
+                tp.setCurrentHour(19);
+                tp.setCurrentMinute(30);
+
+
+
+
+            }
+        });
+
+
+
 
     }
 }
